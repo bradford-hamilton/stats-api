346e819a1d5fd3b191dffe7627dfcff521afcb5a
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2023-07-24 14:55:26 -0600

    internal/server: remove check for GET request, chi already handles a 405 when defining the route as I did: “a.Mux.Get("/ping", a.ping)”

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -3,9 +3,5 @@ package server
 import "net/http"
 
 func (a *API) ping(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
 	w.Write([]byte("pong"))
 }
