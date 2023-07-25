0d4883cf441cfc23baf2a055474006638a76e3a1
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2023-07-25 11:42:31 -0600

    internal/server: first pass of updating schedule handler to complete filtering rules

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -69,24 +69,52 @@ func (a *API) getSchedule(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Filter results based on requirements using teamID
+	// Filter results based on requirements using teamID.
 	if len(s.Dates) == 0 {
-		// Something went wrong/hard to define without MLB API docs
+		// Something went wrong/hard to define without MLB API docs.
 		fmt.Println("TODO, len(s.Dates) == 0")
+		fmt.Println("TODO No games this day?")
 		return
 	}
 
 	games := s.Dates[0].Games
 	if len(games) == 0 {
-		// Something went wrong/hard to define without MLB API docs
+		// Something went wrong/hard to define without MLB API docs.
 		fmt.Println("TODO, len(games) == 0")
 		return
 	}
 
-	// Sort by name, preserving original order
-	sort.SliceStable(games, func(i, _ int) bool {
-		return games[i].Teams.Away.Team.ID == teamIDInt || games[i].Teams.Home.Team.ID == teamIDInt
-	})
+	// Sort by favorite team's ID in home or away games, preserving original order.
+	less := func(i, _ int) bool {
+		return games[i].Teams.Away.Team.ID == teamIDInt ||
+			games[i].Teams.Home.Team.ID == teamIDInt
+	}
+	sort.SliceStable(games, less)
+
+	// // If we matched the favorite teamID and that team has a double header
+	favTeamDoubleHeader := (games[0].Teams.Away.Team.ID == teamIDInt ||
+		games[0].Teams.Home.Team.ID == teamIDInt) &&
+		games[0].DoubleHeader != "N"
+
+	if favTeamDoubleHeader {
+		if len(games) < 2 {
+			// Something odd happened, but don't crash TODO
+			fmt.Printf("len(games) == %d\n", len(games))
+		}
+
+		if games[0].DoubleHeader == "Y" && games[0].Status.StartTimeTBD {
+			// If "single admission"/"traditional" doubleheader type,
+			// sort games 1 and 2 chronologically using "startTimeTBD"
+			games[0], games[1] = games[1], games[0]
+		} else if games[0].DoubleHeader == "S" && (games[0].GameDate.After(games[1].GameDate)) {
+			// If "split admission" doubleheader type, sort games
+			// 1 and 2 chronologically using "gameDate"
+			games[0], games[1] = games[1], games[0]
+		}
+
+		// TODO After those sorts, while still under if favTeamDoubleHeader if the second game is "live", move that up to games[0]
+
+	}
 
 	render.JSON(w, r, s)
 }
